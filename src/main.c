d4a7d6bb141eae46ff5c995cc4d843b8830263df
Matej Mikuš <mikusmatej8@gmail.com>
2025-10-22 13:31:32 +0200

    feat(main): add main function for interpreter entry point

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 /**
  * @file main.c
- * @author xcernoj00
+ * @author xcernoj00,xmikusm00
  * @brief Main function of interpret
  */
 
 #include "error.h"
 #include "scanner.h"
 #include <stdio.h>
+#include "parser.h"
 
 int main() {
 
@@ -14,8 +15,13 @@ int main() {
     source_file = stdin;
 
     set_source_file(source_file);
-
-    print_token_types();
+    int return_code = parser();
+    if (return_code != NO_ERROR){
+        printf("%s,%d\n","error",return_code);
+    }
+    else{
+        printf("%s\n","no_error");
+    }
 
     return NO_ERROR;
 }
