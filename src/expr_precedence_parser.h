c0bb69f05947c08620413db1de28712139e13fa3
Matej Mikus <xmikusm00@vutbr.cz>
2025-11-14 20:46:01 +0100

    feat: add new expr_parser

---
+++ src/expr_precedence_parser.h
@@ -0,0 +1,18 @@
+/**
+ * @file parser.c
+ * @author xmikusm00
+ * @brief expression parser header file
+ */
+#ifndef EXPR_PARSER_H
+#define EXPR_PARSER_H
+#include "scanner.h"
+#include "symtable.h"
+#include "expr_stack.h"
+#include "expr_ast.h"
+
+
+ExprNode*  expression_parser_main( char* first_expr_parser_token, Token *token, int *error_code);
+
+
+
+#endif //EXPR_PARSER_H
