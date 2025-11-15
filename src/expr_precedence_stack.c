5318a819bbfb124afa8b0e3d70fec3174b6313c8
Matej Mikus <xmikusm00@vutbr.cz>
2025-11-15 01:20:24 +0100

    chore: add initial project files for new stack

---
+++ src/expr_precedence_stack.c
@@ -0,0 +1,10 @@
+/**
+ * @file parser.c
+ * @author xmikusm00
+ * @brief Stack implementation for expression evaluation
+ */
+#include "expr_stack.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "error.h"
